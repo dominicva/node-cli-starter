75b13283497f8650896b7d1cfdd4ac7a908c2a9a
Dominic <dominicvana@gmail.com>
2022-04-01 15:59:53 +0100

    Feat: styling added to cli output

--- index.js
+++ index.js
@@ -3,51 +3,60 @@
 import path from 'path';
 import { cwd } from 'process';
 import copy from 'copy-template-dir';
+import chalk from 'chalk';
+import alert from 'cli-alerts';
 
 import init from './utils/init.js';
 import ask from './utils/ask.js';
 
 const { log } = console;
+const { green: g, dim: d } = chalk;
 
 (async () => {
   init();
 
-  const name = await ask({
-    message: 'CLI name?',
-    hint: '(kebab-case only)',
-  });
-  const description = await ask({
-    message: 'CLI description',
-  });
-
-  const version = await ask({
-    message: 'CLI version',
-  });
-
+  const name = await ask({ message: 'CLI name?', hint: '(kebab-case only)' });
+  const description = await ask({ message: 'CLI description?' });
+  const version = await ask({ message: 'CLI version?', initial: '0.0.1' });
   const type = await ask({
-    message: 'module type',
+    message: 'Module type?',
+    initial: 'module',
     hint: 'commonjs or module',
   });
+  const license = await ask({ message: 'CLI license?', initial: 'UNLICENSED' });
+  const authorName = await ask({ message: 'CLI author name?' });
+  const authorEmail = await ask({ message: 'CLI author email?' });
+  const authorURL = await ask({ message: 'CLI author URL?' });
 
   const vars = {
     name,
     description,
     version,
     type,
+    license,
+    authorName,
+    authorEmail,
+    authorURL,
   };
 
-  const inDir = new URL('template', import.meta.url).pathname;
-  const outDir = path.join(cwd(), vars.name);
+  const { name: outDir } = vars;
+  const inDirPath = new URL('template', import.meta.url).pathname;
+  const outDirPath = path.join(cwd(), vars.name);
 
-  copy(inDir, outDir, vars, (err, createdFiles) => {
+  copy(inDirPath, outDirPath, vars, (err, createdFiles) => {
     if (err) throw err;
-    log();
-    log(`Creating files in ./${vars.name}`);
+
+    log(d(`\nCreating files in ${g(`./${outDir}`)} directory\n`));
+
     createdFiles.forEach(filePath => {
       const fileName = path.basename(filePath);
-      log(`Created ${fileName}`);
+      log(`${g('CREATED')} ${fileName}`);
     });
-    log('Done!');
-    log();
+
+    alert({
+      type: 'success',
+      name: 'ALL DONE',
+      msg: `\n\n${createdFiles.length} files created in ${d(`./${outDir}`)} directory`,
+    });
   });
 })();
--- utils/ask.js
+++ utils/ask.js
@@ -3,11 +3,12 @@ import handleError from 'cli-handle-error';
 import enquirer from 'enquirer';
 const { Input } = enquirer;
 
-export default async function ask({ message, hint }) {
+export default async function ask({ message, hint, initial }) {
   const [err, response] = await to(
     new Input({
       message,
       hint,
+      initial,
     }).run()
   );
 
