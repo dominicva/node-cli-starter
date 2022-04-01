32caad1895f987befc8a0c6343f9a717b44c53ff
Dominic <dominicvana@gmail.com>
2022-04-01 17:34:55 +0100

    Refactor: log and generate utils created

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 #!/usr/bin/env node
 
-import path from 'path';
-import { cwd } from 'process';
-import copy from 'copy-template-dir';
-import chalk from 'chalk';
-import alert from 'cli-alerts';
-
 import init from './utils/init.js';
 import cli from './utils/cli.js';
-import questions from './utils/questions.js';
-
-const { log } = console;
-const { green: g, dim: d } = chalk;
+import generate from './utils/generate.js';
+import log from './utils/log.js';
 
 const {
   input,
-  flags: { clear },
+  flags,
+  flags: { clear, debug },
   showHelp,
 } = cli;
 
@@ -23,30 +16,7 @@ const {
   init({ clear });
 
   input.includes('help') && showHelp(0);
+  debug && log(flags);
 
-  // Gather all the user input into an object
-  const vars = await questions();
-
-  // Determine correct file/directory paths/names
-  const { name: outDir } = vars;
-  const inDirPath = new URL('template', import.meta.url).pathname;
-  const outDirPath = path.join(cwd(), outDir);
-
-  // Copy files in template dir to outDir passing-in user input (vars object)
-  copy(inDirPath, outDirPath, vars, (err, createdFiles) => {
-    if (err) throw err;
-
-    log(d(`\nCreating files in ${g(`./${outDir}`)} directory\n`));
-
-    for (const filePath of createdFiles) {
-      const fileName = path.basename(filePath);
-      log(`${g('CREATED')} ${fileName}`);
-    }
-
-    alert({
-      type: 'success',
-      name: 'ALL DONE',
-      msg: `\n\n${createdFiles.length} files created in ${d(`./${outDir}`)} directory`,
-    });
-  });
+  await generate();
 })();
---
+++ utils/generate.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import { cwd } from 'process';
+import copy from 'copy-template-dir';
+import alert from 'cli-alerts';
+import chalk from 'chalk';
+import questions from './questions.js';
+
+const { green: g, dim: d } = chalk;
+const { log } = console;
+
+export default async function generate() {
+  // Gather all the user input into an object
+  const vars = await questions();
+
+  // Determine correct file/directory paths/names
+  const { name: outDir } = vars;
+  const inDirPath = new URL('template', path.join(import.meta.url, '..')).pathname;
+  const outDirPath = path.join(cwd(), outDir);
+
+  // Copy files in template dir to outDir passing-in user input (vars object)
+  copy(inDirPath, outDirPath, vars, (err, createdFiles) => {
+    if (err) throw err;
+
+    log(d(`\nCreating files in ${g(`./${outDir}`)} directory\n`));
+
+    for (const filePath of createdFiles) {
+      const fileName = path.basename(filePath);
+      log(`${g('CREATED')} ${fileName}`);
+    }
+
+    alert({
+      type: 'success',
+      name: 'ALL DONE',
+      msg: `\n\n${createdFiles.length} files created in ${d(`./${outDir}`)} directory`,
+    });
+  });
+}
